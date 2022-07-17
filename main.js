425d5bd0d9b30bf5f9fb3488118efd4e008e3e0c
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2022-07-17 13:08:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ function CaptureSnap(){
     function modelLoaded(){
         console.log('model.is.loaded')
     }
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3)
+    }
+}
